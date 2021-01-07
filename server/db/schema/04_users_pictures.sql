6c36b6ec7d2c70fb6cfc871d3c042ec9fa22f1d7
RicardoJBOF <ricardo.jbof@hotmail.com>
2021-01-07 22:39:53 +0000

    Fix database and seeds

--- server/db/schema/04_users_pictures.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS users_pictures CASCADE;
-
-CREATE TABLE users_pictures(
-    id SERIAL PRIMARY KEY NOT NULL,
-    user_id INTEGER REFERENCES users(id),
-    picture_id INTEGER REFERENCES pictures(id)
-);
