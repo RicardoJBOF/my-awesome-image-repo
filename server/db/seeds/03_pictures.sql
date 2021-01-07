28877a4ab2c31825b77ad9811bd2a631edfb57ce
RicardoJBOF <ricardo.jbof@hotmail.com>
2021-01-07 22:28:25 +0000

    pictures seed

---
+++ server/db/seeds/03_pictures.sql
@@ -0,0 +1,19 @@
+INSERT INTO
+pictures
+  (title, link, saved_time)
+VALUES
+  (
+    'Beach',
+    'https://images.unsplash.com/photo-1607530775692-3abd5a040b13?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80',
+    now()
+    ),
+  (
+    'Notebook',
+    'https://images.unsplash.com/photo-1607522792643-80089ac9da10?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=634&q=80',
+    now()
+    ),
+      (
+    'Bike',
+    'https://images.unsplash.com/photo-1607512047548-6326a3f17c0b?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1314&q=80',
+    now()
+    );
