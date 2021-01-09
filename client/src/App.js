d2bd1ff58686d9203f84ecad84cb259c4567a6a7
RicardoJBOF <ricardo.jbof@hotmail.com>
2021-01-08 20:08:42 -0500

    Add routes frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,60 @@
 import useApplicationData from "./hooks/useApplicationData.js";
 
-const App = () => {
-  const { state, dispatch } = useApplicationData();
-  const userList = state.users.map((user) => (
-    <li key={user.id}>
-      {" "}
-      {user.first_name} {user.last_name} {user.email}{" "}
-    </li>
-  ));
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-  
+import "./App.css";
+
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+export default function App() {
   return (
-    <div className="App">
-      <h1> Users </h1>
-
-      <ul> {userList} </ul>
-    </div>
+    <Router>
+      <ScrollToTop />
+      <div>
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+            {" "}
+            <Home />{" "}
+          </Route>
+          <Route path="/about">
+            {" "}
+            <About />{" "}
+          </Route>
+          <Route path="/login">
+            {" "}
+            <Login />{" "}
+          </Route>
+          <Route path="/register">
+            {" "}
+            <Register />{" "}
+          </Route>
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
   );
-};
+}
 
-export default App;
+// const App = () => {
+//   const { state, dispatch } = useApplicationData();
+//   const userList = state.users.map((user) => (
+//     <li key={user.id}>
+//       {" "}
+//       {user.first_name} {user.last_name} {user.email}{" "}
+//     </li>
+//   ));
+
+//   return (
+//     <div className="App">
+//       <h1> Users </h1>
+
+//       <ul> {userList} </ul>
+//     </div>
+//   );
+// };
+
+// export default App;
