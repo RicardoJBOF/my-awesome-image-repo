d2b27cdae44e59917c09afb8d881f036642a6057
RicardoJBOF <ricardo.jbof@hotmail.com>
2021-01-09 15:49:18 -0500

    React routes working

--- client/src/App.js
+++ client/src/App.js
@@ -5,45 +5,42 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
-// import Login from "./pages/Login";
-// import Register from "./pages/Register";
-// import About from "./pages/About";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import About from "./pages/About";
 
 export default function App() {
   return (
     
-    <div>
-    <Home />
-    </div>
 
-    // <Router>
-    //   <div>
+    <Router>
+      <div>
+        <Navbar />
+        
+        <Switch> 
 
-    //      <Navbar />
-    //     <Switch> 
+          <Route exact path="/">
+            <Home />
+          </Route>
 
-    //       <Route exact path="/">
-    //       <Home />
-    //       </Route>
+          <Route path="/about">
+            <About />
+          </Route>
 
-    //        <Route path="/about">
-    //         {" "}
-    //         <About />{" "}
-    //       </Route>
-    //       <Route path="/login">
-    //         {" "}
-    //         <Login />{" "}
-    //       </Route>
-    //       <Route path="/register">
-    //         {" "}
-    //         <Register />{" "}
-    //       </Route>
-    //     </Switch>
-    //     <Footer /> 
+          <Route path="/login">
+            <Login />
+          </Route>
 
-    //   </div>
-    // </Router>
+          <Route path="/register">
+            <Register />
+          </Route>
 
+        </Switch>
 
+        
+
+        <Footer /> 
+      </div>
+    </Router>
   );
 }
---
+++ client/src/components/navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Navbar() {
+
+  return (
+    <>
+      <nav>
+         <p>NAVBAR DESCRIPTION</p>
+      </nav>
+    </>
+  )
+};
