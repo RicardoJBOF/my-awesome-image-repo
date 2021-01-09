de511df90c3cfa2a35abbbf51dc49a15937a0647
RicardoJBOF <ricardo.jbof@hotmail.com>
2021-01-09 15:35:50 -0500

    Create pages and components skeleton

--- client/src/App.js
+++ client/src/App.js
@@ -1,60 +1,49 @@
-import useApplicationData from "./hooks/useApplicationData.js";
-
+import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
 import "./App.css";
 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
+import Home from "./pages/Home";
+// import Login from "./pages/Login";
+// import Register from "./pages/Register";
+// import About from "./pages/About";
 
 export default function App() {
   return (
-    <Router>
-      <ScrollToTop />
-      <div>
-        <Navbar />
-        <Switch>
-          <Route exact path="/">
-            {" "}
-            <Home />{" "}
-          </Route>
-          <Route path="/about">
-            {" "}
-            <About />{" "}
-          </Route>
-          <Route path="/login">
-            {" "}
-            <Login />{" "}
-          </Route>
-          <Route path="/register">
-            {" "}
-            <Register />{" "}
-          </Route>
-        </Switch>
-        <Footer />
-      </div>
-    </Router>
+    
+    <div>
+    <Home />
+    </div>
+
+    // <Router>
+    //   <div>
+
+    //      <Navbar />
+    //     <Switch> 
+
+    //       <Route exact path="/">
+    //       <Home />
+    //       </Route>
+
+    //        <Route path="/about">
+    //         {" "}
+    //         <About />{" "}
+    //       </Route>
+    //       <Route path="/login">
+    //         {" "}
+    //         <Login />{" "}
+    //       </Route>
+    //       <Route path="/register">
+    //         {" "}
+    //         <Register />{" "}
+    //       </Route>
+    //     </Switch>
+    //     <Footer /> 
+
+    //   </div>
+    // </Router>
+
+
   );
 }
-
-// const App = () => {
-//   const { state, dispatch } = useApplicationData();
-//   const userList = state.users.map((user) => (
-//     <li key={user.id}>
-//       {" "}
-//       {user.first_name} {user.last_name} {user.email}{" "}
-//     </li>
-//   ));
-
-//   return (
-//     <div className="App">
-//       <h1> Users </h1>
-
-//       <ul> {userList} </ul>
-//     </div>
-//   );
-// };
-
-// export default App;
---
+++ client/src/pages/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+import useApplicationData from "../hooks/useApplicationData.js";
+
+export default function Home() {
+  const { state, dispatch } = useApplicationData();
+  const userList = state.users.map((user) => (
+    <li key={user.id}>
+      {" "}
+      {user.first_name} {user.last_name} {user.email}{" "}
+    </li>
+  ));
+
+  return (
+    <div className="App">
+      <h1> Users </h1>
+      <ul> {userList} </ul>
+    </div>
+  );
+}
