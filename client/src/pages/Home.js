d04ea8f807eb8c578f051c547efa9ba15ee5dd5a
RicardoJBOF <ricardo.jbof@hotmail.com>
2021-01-14 15:42:19 -0500

    Create SinglePicture component hard code

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import useApplicationData from "../hooks/useApplicationData.js";
 import { Link } from "react-router-dom";
+import SinglePicture from "../components/SinglePicture.js"
 
 export default function Home() {
   const { state } = useApplicationData();
@@ -16,18 +17,26 @@ export default function Home() {
   return (
     <div className="App">
 
-      <h1> Users </h1>
-      <ul> {userList} </ul>
+      <h1> MY AWESOME IMAGE REPO </h1>
+      <p> {userList} </p>
+      <SinglePicture 
+        id="1"
+        title="Model"
+        link="https://images.unsplash.com/photo-1607530775692-3abd5a040b13?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80"
+        saved_time="01/01/2000"
+      />
 
-      {user ?
-        <Link to="/pictures/new" className="LoginRegister_btn">
-        Add Picture
-        </Link> 
-        :
-        <Link to="/login" className="LoginRegister_btn">
-        Login to Add Picture
-        </Link> 
-      } 
+      <div>
+        {user ?
+          <Link to="/pictures/new" className="LoginRegister_btn">
+          Add Picture
+          </Link> 
+          :
+          <Link to="/login" className="LoginRegister_btn">
+          Login to Add Picture
+          </Link> 
+        } 
+      </div>
 
 
 
